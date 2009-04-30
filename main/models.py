a3c7091b9391c06e2c259096e22081fe560b2e96
tonio <devnull@localhost>
2009-04-30 18:11:48 +0200

    New model for auto-reply messages handling.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from mailng.admin.models import Mailbox
 
-# Create your models here.
+class ARmessage(models.Model):
+    mbox = models.ForeignKey(Mailbox)
+    last_sent = models.DateField()
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
