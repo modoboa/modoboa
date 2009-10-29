54ba7ed40eb1bf465e8e8ca8a8d0ec63cce95708
tonio <devnull@localhost>
2009-10-29 09:56:52 +0100

    Documentation update. New generic way to specify templates directory (no more hard coded absolute path).

--- settings_sample.py
+++ settings_sample.py
@@ -1,4 +1,5 @@
 # Django settings for mailng project.
+import os.path
 
 DEBUG = False 
 TEMPLATE_DEBUG = DEBUG
@@ -71,6 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
 )
 
 INSTALLED_APPS = (
