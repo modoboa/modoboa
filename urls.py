9894ca9b04f8ca6c0b6102d479b3968cfe039463
Tonio <tonio@ngyn.org>
2010-07-18 12:12:53 +0200

    wip: #69: new progressbar gif, better display, new code position.

--- urls.py
+++ urls.py
@@ -1,39 +1,24 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 from modoboa.extensions import *
 from modoboa.lib import parameters
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^modoboa/', include('modoboa.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
-
     (r'^modoboa/admin/', include('modoboa.admin.urls')),
     (r'^modoboa/userprefs/', include('modoboa.userprefs.urls')),
     (r'^accounts/login/$', 'modoboa.auth.views.dologin'),
     (r'^accounts/logout/$', 'modoboa.auth.views.dologout'),
     (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', 
      {'packages': ('modoboa',),}),
-
     *loadextensions()
 )
 
 if settings.DEBUG:
+    print "debug"
     urlpatterns += patterns(
         '',
         (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.STATIC_ROOTDIR})
+         {'document_root': settings.MEDIA_ROOT})
         )
     # Stats
     if isenabled('stats'):
