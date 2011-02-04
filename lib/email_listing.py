24431b34393f17240d12fe55f7ce4615178bac8b
Antoine Nguyen <tonio@ngyn.org>
2011-02-04 10:39:47 +0100

    fixed #80
    The real problem was due to the use of a <pre> tag to display plaintext messages. By default, it does not automatically cut long lines as with other tags. Some CSS stuff fixed the problem.
    
    Fixed bad behaviour on some combinations between display mode and message available formats. (ie. plain/text or plain/html)

--- lib/email_listing.py
+++ lib/email_listing.py
@@ -152,6 +152,8 @@ class Email(object):
 
         self.__parse(msg)
 
+        print "mf : %s, df : %s" % (mformat, dformat)
+
         if not self.contents.has_key(mformat) or self.contents[mformat] == "":
             # Fallback
             self.mformat = mformat == "html" and "plain" or "html"
@@ -280,8 +282,7 @@ class Email(object):
                 })
         
     def viewmail_plain(self, content, **kwargs):
-        return self.dformat == "html" and "<pre>%s</pre>" % content \
-            or content
+        return "<pre>%s</pre>" % content
 
     def viewmail_html(self, content, **kwargs):
         if content is None or content == "":
