9716ae9dca1e8efc3aceadd789cfda99aca202c8
Antoine Nguyen <tonio@ngyn.org>
2012-07-04 15:20:32 +0200

    Added useful comments to the example settings.py file.

--- example/settings.py
+++ example/settings.py
@@ -14,17 +14,17 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default" : {
-        "ENGINE" : "django.db.backends.",
-        "NAME" : "modoboa",
-        "USER" : "",
-        "PASSWORD" : "",
-        "HOST" : "",
-        "PORT" : "",
-        # MySQL users only
-        # "OPTIONS" : {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+	# MySQL users only
+        # 'OPTIONS' : {
         #     "init_command" : "SET foreign_key_checks = 0;",
         # },
-    },
+    }
     # "pfxadmin" : {
     #     "ENGINE" : "django.db.backends.",
     #     "NAME" : "",
@@ -47,7 +47,7 @@ DATABASE_ROUTERS = ["modoboa.extensions.amavis.dbrouter.AmavisRouter"]
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Paris'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
