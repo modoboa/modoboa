7a4b91690c9cc245a5b7a9cfa30cd669fb72b131
Antoine Nguyen <tonio@ngyn.org>
2012-10-09 21:11:01 +0200

    Added portuguese to the list of available languages.
    fix #313

--- modoboa/userprefs/__init__.py
+++ modoboa/userprefs/__init__.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy
 parameters.register_user("LANG", type="list", deflt="en", 
                          label=ugettext_lazy("Prefered language"),
                          values=[("de", "deutsch"), ("en", "english"),
-                                 ("es", "español"), ("fr", "français")],
+                                 ("es", "español"), ("fr", "français"),
+                                 ("pt", "português")],
                          help=ugettext_lazy("Prefered language to display pages"),
                          app="general")
