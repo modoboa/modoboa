81e6a3567ae8164d51c3efd81d69dbdbd836db59
Antoine Nguyen <tonio@ngyn.org>
2013-05-10 09:58:15 +0200

    Default language for new users now respects LANGUAGE_CODE.
    fixes #385

--- modoboa/userprefs/app_settings.py
+++ modoboa/userprefs/app_settings.py
@@ -1,14 +1,21 @@
 # coding: utf-8
 from django import forms
 from django.utils.translation import ugettext_lazy
+from django.conf import settings
 from modoboa.lib.parameters import UserParametersForm
+
+
+def translate_language_code(value):
+    if "-" in value:
+        return value.split("-")[0]
+    return value
 
 
 class UserSettings(UserParametersForm):
     app = "general"
 
     lang = forms.ChoiceField(
-        initial="en",
+        initial=translate_language_code(settings.LANGUAGE_CODE),
         label=ugettext_lazy("Prefered language"),
         choices=[("de", "deutsch"), ("en", "english"),
                 ("es", "español"), ("fr", "français"),
