73bc0dbee54b14d196797826cfd03e6165e5d117
Antoine Nguyen <tonio@ngyn.org>
2013-05-18 10:46:43 +0200

    Adding reversion to INSTALLED_APPS.
    see #320

--- modoboa/core/templates/settings.py
+++ modoboa/core/templates/settings.py
@@ -135,7 +135,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-
+    'reversion',
     'south',
     'modoboa',
     'modoboa.auth',
