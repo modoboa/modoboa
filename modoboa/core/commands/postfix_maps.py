b15e76d1c40460b2541e164cd150f12b8f68b4bd
Antoine Nguyen <tonio@ngyn.org>
2013-06-12 11:42:25 +0200

    Queries update.

--- modoboa/core/commands/postfix_maps.py
+++ modoboa/core/commands/postfix_maps.py
@@ -39,8 +39,8 @@ class DomainsAliasesMap(MapFile):
 
 class MailboxesMap(MapFile):
     filename = 'sql-mailboxes.cf'
-    mysql = "SELECT concat(dom.name, '/', mb.path) FROM admin_mailbox mb INNER JOIN admin_domain dom ON mb.domain_id=dom.id INNER JOIN admin_user user ON mb.user_id=user.id WHERE dom.enabled=1 AND dom.name='%d' AND user.is_active=1 AND mb.address='%u'"
-    postgres = "SELECT dom.name || '/' || mb.path FROM admin_mailbox mb INNER JOIN admin_domain dom ON mb.domain_id=dom.id INNER JOIN admin_user u ON mb.user_id=u.id WHERE dom.enabled AND dom.name='%d' AND u.is_active AND mb.address='%u'"
+    mysql = "SELECT 1 FROM admin_mailbox mb INNER JOIN admin_domain dom ON mb.domain_id=dom.id INNER JOIN admin_user user ON mb.user_id=user.id WHERE dom.enabled=1 AND dom.name='%d' AND user.is_active=1 AND mb.address='%u'"
+    postgres = "SELECT 1 FROM admin_mailbox mb INNER JOIN admin_domain dom ON mb.domain_id=dom.id INNER JOIN admin_user u ON mb.user_id=u.id WHERE dom.enabled AND dom.name='%d' AND u.is_active AND mb.address='%u'"
 
 class AliasesMap(MapFile):
     filename = 'sql-aliases.cf'
