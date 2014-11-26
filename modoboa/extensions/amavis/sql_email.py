1ba92ce862d72bf28db9f8ac806b7dc93ce78869
Antoine Nguyen <tonio@ngyn.org>
2014-11-26 19:35:46 +0100

    Convert to string before calling the method.
    see #643
    
    Conflicts:
    	modoboa/extensions/amavis/sql_listing.py

--- modoboa/extensions/amavis/sql_email.py
+++ modoboa/extensions/amavis/sql_email.py
@@ -36,15 +36,13 @@ class SQLemail(Email):
 
     @property
     def msg(self):
-        """
-        """
+        """Get message's content."""
         import email
 
         if self._msg is None:
             qmails = get_connector().get_mail_content(self.mailid)
-            self._msg = email.message_from_string(
-                "".join([qm.mail_text for qm in qmails])
-            )
+            mail_text = "".join([qm.mail_text for qm in qmails])
+            self._msg = email.message_from_string(mail_text.encode("utf-8"))
             self._parse(self._msg)
         return self._msg
 
