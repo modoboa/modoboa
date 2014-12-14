8fb2a1530fc3bdab60426372ff154bba6b74d944
Antoine Nguyen <tonio@ngyn.org>
2014-12-14 11:49:36 +0100

    New translation: pt_BR

--- modoboa/core/app_settings.py
+++ modoboa/core/app_settings.py
@@ -370,11 +370,17 @@ class UserSettings(parameters.UserParametersForm):
     lang = forms.ChoiceField(
         initial=translate_language_code(settings.LANGUAGE_CODE),
         label=ugettext_lazy("Prefered language"),
-        choices=[("cs", "čeština"), ("de", "deutsch"),
-                 ("en", "english"), ("es", "español"),
-                 ("fr", "français"), ("it", "italiano"),
-                 ("nl", "nederlands"), ("pt", "português"),
-                 ("ru", "русский"), ("sv", "svenska")],
+        choices=[("cs", "čeština"),
+                 ("de", "deutsch"),
+                 ("en", "english"),
+                 ("es", "español"),
+                 ("fr", "français"),
+                 ("it", "italiano"),
+                 ("nl", "nederlands"),
+                 ("pt_PT", "português"),
+                 ("pt_BR", "português (BR)"),
+                 ("ru", "русский"),
+                 ("sv", "svenska")],
         help_text=ugettext_lazy("Prefered language to display pages"),
         widget=forms.Select(attrs={"class": "form-control"})
     )
