a1d65da0bd9d29698737164b728baa9097a72198
Antoine Nguyen <tonio@ngyn.org>
2014-12-16 16:41:13 +0100

    Convert to string only if necessary

--- modoboa/extensions/amavis/sql_email.py
+++ modoboa/extensions/amavis/sql_email.py
@@ -42,7 +42,9 @@ class SQLemail(Email):
         if self._msg is None:
             qmails = get_connector().get_mail_content(self.mailid)
             mail_text = "".join([qm.mail_text for qm in qmails])
-            self._msg = email.message_from_string(mail_text.encode("utf-8"))
+            if type(mail_text) is unicode:
+                mail_text = mail_text.encode("utf-8")
+            self._msg = email.message_from_string(mail_text)
             self._parse(self._msg)
         return self._msg
 
