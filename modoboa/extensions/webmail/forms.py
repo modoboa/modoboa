df16a3296d5ca4c976b80b7b87a341ae6b4f8aec
Antoine Nguyen <tonio@ngyn.org>
2015-03-11 10:25:27 +0100

    PEP8 fixes

--- modoboa/extensions/webmail/forms.py
+++ modoboa/extensions/webmail/forms.py
@@ -83,9 +83,12 @@ class ComposeMailForm(forms.Form):
 
     """Compose mail form."""
 
-    to = forms.CharField(label=_("To"), validators=[validate_email_list])
-    cc = forms.CharField(label=_("Cc"), required=False, validators=[validate_email_list])
-    cci = forms.CharField(label=_("Cci"), required=False, validators=[validate_email_list])
+    to = forms.CharField(
+        label=_("To"), validators=[validate_email_list])
+    cc = forms.CharField(
+        label=_("Cc"), required=False, validators=[validate_email_list])
+    cci = forms.CharField(
+        label=_("Cci"), required=False, validators=[validate_email_list])
     subject = forms.CharField(
         label=_("Subject"),
         max_length=255,
