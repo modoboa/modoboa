50ca1d9cbb936e8688d4d88620e1a649ae481508
Antoine Nguyen <tonio@ngyn.org>
2015-04-15 11:16:03 +0200

    Translations updated.

--- modoboa/demo/urls.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns(
-    'modoboa.demo.views',
-
-    url(r'^sendvirus/$', 'send_virus', name="virus_send"),
-    url(r'^sendspam/$', 'send_spam', name="spam_send"),
-)
