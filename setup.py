24ece682de9e4b1b2aad2662f31858ed1c07e5c3
Antoine Nguyen <tonio@ngyn.org>
2015-06-11 19:43:52 +0200

    1.3.2.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def read(fname):
 
 setup(
     name="modoboa",
-    version='1.3.1',
+    version='1.3.2',
     url='http://modoboa.org/',
     license='MIT',
     description="Mail hosting made simple",
