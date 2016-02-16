87447e56f8cf234732ce4bc5dfe607556c49f4e1
Antoine Nguyen <tonio@ngyn.org>
2016-02-16 16:20:23 +0100

    Use django's API...
    see #835

--- modoboa/lib/db_utils.py
+++ modoboa/lib/db_utils.py
@@ -7,23 +7,9 @@ from django.utils.translation import ugettext as _
 from modoboa.lib.exceptions import InternalError
 
 
-def db_table_exists(table, cursor=None):
-    """Check if table exists
-
-    Taken from here:
-    https://gist.github.com/527113/307c2dec09ceeb647b8fa1d6d49591f3352cb034
-
-    """
-    try:
-        if not cursor:
-            cursor = connection.cursor()
-        if not cursor:
-            raise Exception
-        table_names = connection.introspection.get_table_list(cursor)
-    except:
-        raise Exception("unable to determine if the table '%s' exists" % table)
-    else:
-        return table in table_names
+def db_table_exists(table):
+    """Check if table exists."""
+    return table in connection.introspection.table_names()
 
 
 def db_type(cname="default"):
