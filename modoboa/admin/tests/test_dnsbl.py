4db98c0739ddb06efb8b31fae1140b811b2fbee3
Antoine Nguyen <tonio@ngyn.org>
2016-06-08 18:16:12 +0200

    Increase coverage.
    see #879

--- modoboa/admin/tests/test_dnsbl.py
+++ modoboa/admin/tests/test_dnsbl.py
@@ -1,6 +1,7 @@
 """DNSBL related tests."""
 
 from django.core import management
+from django.core.urlresolvers import reverse
 from django.test import override_settings
 
 from modoboa.lib.tests import ModoTestCase
@@ -16,12 +17,16 @@ class DNSBLTestCase(ModoTestCase):
     def setUpTestData(cls):
         """Create some data."""
         super(DNSBLTestCase, cls).setUpTestData()
-        factories.DomainFactory(name="modoboa.org")
+        cls.domain = factories.DomainFactory(name="modoboa.org")
+        factories.DomainFactory(name="pouet.com")  # should not exist
 
     @override_settings(DNSBL_PROVIDERS=["zen.spamhaus.org"])
     def test_management_command(self):
         """Check that command works fine."""
+        self.assertEqual(models.DNSBLResult.objects.count(), 0)
         management.call_command("modo", "check_dnsbl")
         self.assertTrue(
-            models.DNSBLResult.objects.filter(
-                domain__name="modoboa.org").exists())
+            models.DNSBLResult.objects.filter(domain=self.domain).exists())
+        response = self.client.get(
+            reverse("admin:dnsbl_domain_detail", args=[self.domain.pk]))
+        self.assertEqual(response.status_code, 200)
