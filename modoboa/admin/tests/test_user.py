9813893a05c2906658cb4ea05ec469ea721914b9
Antoine Nguyen <tonio@ngyn.org>
2016-06-08 11:15:34 +0200

    Increase coverage.
    see #878

--- modoboa/admin/tests/test_user.py
+++ modoboa/admin/tests/test_user.py
@@ -1,3 +1,7 @@
+"""User related tests."""
+
+import json
+
 from django.core.urlresolvers import reverse
 
 from modoboa.core.models import User
@@ -8,18 +12,25 @@ from ..models import Alias
 
 
 class ForwardTestCase(ModoTestCase):
+    """User forward test cases."""
 
     def setUp(self):
         super(ForwardTestCase, self).setUp()
         populate_database()
 
-    def test_forward_permissions(self):
+    def test_set_forward(self):
         self.client.logout()
         self.client.login(username='user@test.com', password='toto')
+        url = reverse('user_forward')
         self.ajax_post(
-            reverse('user_forward'),
+            url,
             {'dest': 'user@extdomain.com', 'keepcopies': True}
         )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        self.assertIn('user@extdomain.com', content["content"])
+        self.assertNotIn('user@test.com', content["content"])
         forward = Alias.objects.get(address='user@test.com', internal=False)
         sadmin = User.objects.get(username='admin')
         self.assertTrue(sadmin.can_access(forward))
