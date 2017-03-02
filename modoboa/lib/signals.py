649465413b8aec166cfa55ee309cb111cba136e1
Antoine Nguyen <tonio@ngyn.org>
2017-03-02 11:34:22 +0100

    Make sure signal is not shared between threads.
    see #1015

--- modoboa/lib/signals.py
+++ modoboa/lib/signals.py
@@ -1,13 +1,16 @@
 """Custom signals."""
 
+from threading import local
+
 import django.dispatch
 
-request_accessor = django.dispatch.Signal()
+request_accessor = local()
+request_accessor.signal = django.dispatch.Signal()
 
 
 def get_request():
     """Get the current request from anywhere."""
-    request = request_accessor.send(None)
+    request = request_accessor.signal.send(None)
     if request:
         return request[0][1]
     return None
