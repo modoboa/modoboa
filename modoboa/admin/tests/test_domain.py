29336209ef854a32f0591b61e98cd1f7346d672f
Antoine Nguyen <tonio@ngyn.org>
2017-06-16 16:06:35 +0200

    Fixed unit test.
    see #1138

--- modoboa/admin/tests/test_domain.py
+++ modoboa/admin/tests/test_domain.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-from __future__ import unicode_literals
-
 """Domain related test cases."""
+
+from __future__ import unicode_literals
 
 from django.core.urlresolvers import reverse
 
