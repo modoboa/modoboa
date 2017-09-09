0696d51bd9e2c97f874fe09b30f2d3d02aa3829d
Antoine Nguyen <tonio@ngyn.org>
2017-09-09 09:12:29 +0200

    Released 1.9.0

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ def read(fname):
 
 setup(
     name="modoboa",
-    version="1.8.3",
+    version="1.9.0",
     url="http://modoboa.org/",
     license="ISC",
     description="Mail hosting made simple",
