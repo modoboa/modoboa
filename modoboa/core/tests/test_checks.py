dcabbf9440dc64b54e3a391016374abcf4c6522a
Andrew Fyfe <andrew@neptune-one.net>
2018-01-09 15:29:51 +0000

    create a deployment check to ensure USE_TZ is enabled
    Fixes #1086

---
+++ modoboa/core/tests/test_checks.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase
+from django.test.utils import override_settings
+
+from ..checks import settings_checks
+
+
+class CheckSessionCookieSecureTest(SimpleTestCase):
+    @override_settings(USE_TZ=False)
+    def test_use_tz_false(self):
+        """If USE_TZ is off provide one warning."""
+        self.assertEqual(
+            settings_checks.check_use_tz_enabled(None),
+            [settings_checks.W001]
+        )
+
+    @override_settings(USE_TZ=True)
+    def test_use_tz_true(self):
+        """If USE_TZ is on, there's no warning about it."""
+        self.assertEqual(settings_checks.check_use_tz_enabled(None), [])
