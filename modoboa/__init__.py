50da7af6bad46cd5522dbee4712304c56fe87a50
Andrew Fyfe <andrew@neptune-one.net>
2018-01-11 19:05:12 +0000

    refactor setup.py
    - use setuptools_scm to generate version number, normal releases will
    use the tagged version (ie 1.9.1), versions installed from source will
    append the git commit number to the version number (ie
    1.9.1.dev61+g4584c93)
    
    - use io.open() to read text files to correctly handle utf-8 characters
    
    - use pip to parse requirements.txt
    
    - update classifiers to specify python and django versions supported
    
    - fix classifier for license
    
    - wheel distributions can now be built using `python setup.py bdist_wheel`

--- modoboa/__init__.py
+++ modoboa/__init__.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Modoboa - Mail hosting made simple."""
+
 from __future__ import unicode_literals
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
