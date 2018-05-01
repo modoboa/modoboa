b194f0cb560794d4478d09fa2c829fff94613826
Gui Iribarren <guido@codigosur.org>
2018-05-01 10:46:31 +0200

    Convert options[sepchar] into string. Fixes #1483
    Signed-off-by: Gui Iribarren <guido@codigosur.org>

--- modoboa/admin/management/commands/subcommands/_export.py
+++ modoboa/admin/management/commands/subcommands/_export.py
@@ -60,5 +60,5 @@ class ExportCommand(BaseCommand):
 
     def handle(self, *args, **options):
         exts_pool.load_all()
-        self.csvwriter = csv.writer(sys.stdout, delimiter=options["sepchar"])
+        self.csvwriter = csv.writer(sys.stdout, delimiter=unicode(options["sepchar"]))
         getattr(self, "export_{}".format(options["objtype"]))()
