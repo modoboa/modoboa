d800f178b5ccc834ebe390077f4829ad79db0063
Christophe CHAUVET <christophe.chauvet@gmail.com>
2020-10-27 19:13:10 +0100

    Retrieve PostgreSQL or MySQL port from env var

--- tests.py
+++ tests.py
@@ -5,11 +5,16 @@ import unittest
 from modoboa.lib.sysutils import exec_cmd
 
 DB = os.environ.get("DB", "postgres")
+if DB.lower() == "postgres":
+    PORT = os.environ.get("POSTGRES_PORT", "5432")
+else:
+    PORT = os.environ.get("MYSQL_PORT", "3306")
 
 
 class DeployTest(unittest.TestCase):
     dbtype = DB.lower()
     dbhost = "localhost"
+    dbport = PORT
     projname = "modoboa_test"
     dbuser = DB == "MYSQL" and "modoboa" or "postgres"
     dbpassword = DB == "MYSQL" and "modoboa" or ""
@@ -18,9 +23,9 @@ class DeployTest(unittest.TestCase):
         self.workdir = tempfile.mkdtemp()
 
     def test_silent(self):
-        dburl = "default:%s://%s:%s@%s/%s" \
+        dburl = "default:%s://%s:%s@%s:%s/%s" \
             % (self.dbtype, self.dbuser, self.dbpassword,
-               self.dbhost, self.projname)
+               self.dbhost, self.dbport, self.projname)
         cmd = (
             "modoboa-admin.py deploy --collectstatic "
             "--dburl %s --domain %s --admin-username admin %s"
