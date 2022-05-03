bfb51bcdecb1a18141e156403634cf402d2cba27
Antoine Nguyen <tonio@ngyn.org>
2022-05-03 17:29:09 +0200

    Preparing 2.0 release

--- modoboa/core/api/v2/views.py
+++ modoboa/core/api/v2/views.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from django.utils.html import escape
 from django.utils.translation import ugettext as _
 
 from rest_framework import response, status
@@ -25,7 +26,7 @@ class TokenObtainPairView(jwt_views.TokenObtainPairView):
             logger.warning(
                 _("Failed connection attempt from '%s' as user '%s'"),
                 request.META["REMOTE_ADDR"],
-                serializer.data["username"]
+                escape(serializer.data["username"])
             )
             raise InvalidToken(e.args[0])
 
