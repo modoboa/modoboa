70b3fa5efb0cb52187374c1dd3f129fb902aa066
Spitap <dev@asdrip.fr>
2022-09-05 14:15:31 +0200

    make codecov happy

--- modoboa/admin/tests/test_mx.py
+++ modoboa/admin/tests/test_mx.py
@@ -221,6 +221,19 @@ class DNSBLTestCase(ModoTestCase):
         management.call_command(
             "modo", "check_mx", "--email", "user@example.test")
         self.assertEqual(len(mail.outbox), 2)
+    
+    @mock.patch("gevent.socket.gethostbyname")
+    @mock.patch("socket.getaddrinfo")
+    @mock.patch.object(dns.resolver.Resolver, "resolve")
+    def test_notifications_wrong_dnsbl_response(
+            self, mock_query, mock_getaddrinfo, mock_g_gethostbyname):
+        """Check notifications."""
+        mock_query.side_effect = utils.mock_dns_query_result
+        mock_getaddrinfo.side_effect = utils.mock_ip_query_result
+        mock_g_gethostbyname.return_value = "127.255.255.254" #<--Spamhaus response when querying from an open resolver
+        management.call_command(
+            "modo", "check_mx", "--email", "user@example.test")
+        self.assertEqual(len(mail.outbox), 1)
 
     @mock.patch("gevent.socket.gethostbyname")
     @mock.patch("socket.getaddrinfo")
