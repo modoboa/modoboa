0b2d4f4937b960f0bd75f834972dd2ab4c3e22b0
Spitap <dev@asdrip.fr>
2023-02-06 10:48:06 +0100

    better throttling, doc updated

--- modoboa/lib/throttle.py
+++ modoboa/lib/throttle.py
@@ -1,4 +1,5 @@
-from rest_framework.throttling import SimpleRateThrottle
+from rest_framework.throttling import SimpleRateThrottle, AnonRateThrottle
+from django.urls import resolve
 
 class UserDdosPerView(SimpleRateThrottle):
     """Custom Throttle class for rest_framework. The throttling is applied on a per view basis for authentificated users."""
@@ -11,7 +12,7 @@ class UserDdosPerView(SimpleRateThrottle):
         else:
             ident = self.get_ident(request)
         return self.cache_format % {
-            'scope': hash(request.path),
+            'scope': hash(resolve(request.path).url_name),
             'ident': ident
         }
 
@@ -26,6 +27,36 @@ class UserLesserDdosUser(SimpleRateThrottle):
         else:
             ident = self.get_ident(request)
         return self.cache_format % {
-            'scope': hash(request.path),
+            'scope': hash(resolve(request.path).url_name),
             'ident': ident
         }
+
+class LoginThrottle(SimpleRateThrottle):
+
+    scope = 'login'
+
+    def get_cache_key(self, request, view = None):
+            return self.cache_format % {
+            'scope': self.scope,
+            'ident': self.get_ident(request)
+        }
+
+    def reset_cache(self, request):
+        self.key = self.get_cache_key(request)
+        self.cache.delete(self.key)
+
+
+class PasswordResetRequestThrottle(LoginThrottle):
+
+    scope = 'password_recovery_request'
+
+
+class PasswordResetTotpThrottle(LoginThrottle):
+
+    scope = 'password_recovery_totp_check'
+
+
+class PasswordResetApplyThrottle(LoginThrottle):
+
+    scope = 'password_recovery_apply'
+
