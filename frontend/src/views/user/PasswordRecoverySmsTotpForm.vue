620ec75392e64a3a416a2d7b19ccf0fed1cd5db2
Spitap <dev@asdrip.fr>
2023-02-09 10:52:25 +0100

    fixed typo

--- frontend/src/views/user/PasswordRecoverySmsTotpForm.vue
+++ frontend/src/views/user/PasswordRecoverySmsTotpForm.vue
@@ -82,7 +82,7 @@ export default {
         this.loading = false
         if (resp.status === 200) {
           this.$refs.observer.setErrors({
-            password_confirmed: this.$gettext('TOTP resent.')
+            sms_totp: this.$gettext('TOTP resent.')
           })
         }
       }).catch(err => {
@@ -91,7 +91,7 @@ export default {
           this.showErrorDialog(this.$t('User seems wrong, return to login or restart reset the process?'))
         } else if (err.response.status === 429) {
           this.$refs.observer.setErrors({
-            password: this.$gettext('Too many unsuccessful attempts, please try later.')
+            sms_totp: this.$gettext('Too many unsuccessful attempts, please try later.')
           })
         }
       })
