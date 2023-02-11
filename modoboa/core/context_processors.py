30a8a59906ee727c3e99322f7aeaf280a211bafa
Spitap <dev@asdrip.fr>
2023-02-11 16:41:44 +0100

    Fixed default new-admin url

--- modoboa/core/context_processors.py
+++ modoboa/core/context_processors.py
@@ -25,5 +25,5 @@ def top_notifications(request):
 def new_admin_url(*args):
     """A context processor to include new admin url."""
     return {
-        "new_admin": getattr(settings, "NEW_ADMIN_URL", "new_admin")
+        "new_admin": getattr(settings, "NEW_ADMIN_URL", "new-admin")
     }
