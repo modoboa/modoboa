7775a2163e8091e57817e53153046f6e1447c208
Antoine Nguyen <tonio@ngyn.org>
2023-02-23 11:27:39 +0100

    Added new translations to frontend

--- frontend/merge_translations.py
+++ frontend/merge_translations.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python
 
+import os
+
 import polib
 
 
 LOCALES = [
+    "br",
+    "cs",
+    "de",
+    "el",
+    "es",
+    "fi",
     "fr",
+    "it",
+    "ja",
+    "nl",
+    "pt",
+    "pt-br",
+    "pl",
+    "po",
+    "ru",
+    "sv",
+    "tr",
+    "zh-hant",
 ]
 
 OLD_TRANSLATION_MODULES = [
@@ -22,10 +41,16 @@ OLD_TRANSLATION_MODULES = [
 
 def merge_translations():
     for locale in LOCALES:
-        new_po = polib.pofile(f"locale/{locale}/LC_MESSAGES/app.po")
+        target = f"locale/{locale}/LC_MESSAGES/app.po"
+        print(f"Opening {target}")
+        new_po = polib.pofile(target)
         for module in OLD_TRANSLATION_MODULES:
             prefix = f"../modoboa/{module}" if module else "../modoboa"
-            old_po = polib.pofile(f"{prefix}/locale/{locale}/LC_MESSAGES/django.po")
+            old_file = f"{prefix}/locale/{locale}/LC_MESSAGES/django.po"
+            if not os.path.exists(old_file):
+                continue
+            print(f"Opening {old_file}")
+            old_po = polib.pofile(old_file)
             for entry in new_po:
                 old_entry = old_po.find(entry.msgid)
                 if old_entry:
