99c7de5f3918425d0b7d83f56e4ef1b3a4dfe0f3
Spitap <dev@asdrip.fr>
2023-04-22 18:17:20 +0200

    Print outbox

--- modoboa/core/tests/test_core.py
+++ modoboa/core/tests/test_core.py
@@ -289,4 +289,6 @@ class APICommunicationTestCase(ModoTestCase):
                 mocks.modo_api_instance_update,
                 mocks.modo_api_versions_no_update):
             management.call_command("communicate_with_public_api")
+        for mails in mail.outbox:
+            print(mails.body)
         self.assertEqual(len(mail.outbox), 1)
