d2e70cb62dec4ee901dca9c1aeee14813bb3b004
Spitap <dev@asdrip.fr>
2023-04-22 12:26:37 +0200

    Updating to pyproject.toml

--- modoboa/__init__.py
+++ modoboa/__init__.py
@@ -1,9 +1,13 @@
 """Modoboa - Mail hosting made simple."""
 
 from pkg_resources import DistributionNotFound, get_distribution
+from modoboa.core.commands import handle_command_line
 
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
     # package is not installed
     pass
+
+def modoboa_admin():
+    handle_command_line()
