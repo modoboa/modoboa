044fdd792167d1bec5f0f6b4af5a47706aeda581
Spitap <dev@asdrip.fr>
2023-04-22 17:46:33 +0200

    Updated version

--- modoboa/__init__.py
+++ modoboa/__init__.py
@@ -1,7 +1,6 @@
 """Modoboa - Mail hosting made simple."""
 
 from pkg_resources import DistributionNotFound, get_distribution
-from modoboa.core.commands import handle_command_line
 
 try:
     __version__ = get_distribution(__name__).version
@@ -10,4 +9,5 @@ except DistributionNotFound:
     pass
 
 def modoboa_admin():
+    from modoboa.core.commands import handle_command_line
     handle_command_line()
---
+++ modoboa/version.py
@@ -0,0 +1,10 @@
+import os
+
+def get_version():
+    from setuptools_scm import get_version as default_version
+    github_version = os.environ.get('GITHUB_REF_NAME', None)
+    if github_version is not None:
+        return github_version
+
+    return default_version()
+
