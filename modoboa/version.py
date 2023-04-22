a3e6be4398a4370d7814b7fc49d7b091f75724e8
Spitap <dev@asdrip.fr>
2023-04-22 19:03:30 +0200

    Fixed local scheme

--- modoboa/version.py
+++ modoboa/version.py
@@ -1,10 +1,14 @@
 import os
 
+
+def local_scheme(version):
+    return ""
+
+
 def get_version():
     from setuptools_scm import get_version as default_version
     github_version = os.environ.get('GITHUB_REF_NAME', None)
     if github_version is not None:
         return github_version
-
-    return default_version(local_scheme="")
+    return default_version(local_scheme=local_scheme)
 
