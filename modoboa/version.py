941b4d74e1301163f254f2857ebd5b620c52dd90
Spitap <dev@asdrip.fr>
2023-04-22 19:17:39 +0200

    added debug github version for release

--- modoboa/version.py
+++ modoboa/version.py
@@ -10,6 +10,7 @@ def get_version():
     github_version = os.environ.get('GITHUB_REF_NAME', None)
     is_release = os.environ.get('IS_RELEASE', None)
     if is_release is not None and github_version is not None:
+        print(f"GITHUB_REF_NAME found, using version: {github_version}")
         return github_version
     return default_version(local_scheme=local_scheme)
 
