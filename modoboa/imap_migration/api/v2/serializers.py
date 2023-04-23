49a9a308b93b5ab96d3cc179c91fcf586622ea78
Spitap <dev@asdrip.fr>
2023-04-23 17:13:49 +0200

    Delete imap api v1, better pdfcredentials settings validation

--- modoboa/imap_migration/api/v2/serializers.py
+++ modoboa/imap_migration/api/v2/serializers.py
@@ -3,10 +3,11 @@
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
-from modoboa.admin.models import Mailbox
-from modoboa.imap_migration.api.v1 import serializers as v1_serializers
 
+from modoboa.admin.models import Mailbox
 from modoboa.admin.models.domain import Domain
+
+from modoboa.imap_migration import models
 
 
 class IMAPMigrationSettingsSerializer(serializers.Serializer):
@@ -26,6 +27,73 @@ class IMAPMigrationSettingsSerializer(serializers.Serializer):
     folder_filter_include = serializers.CharField(required=False,
                                                   allow_blank=True,
                                                   default="")
+
+
+class EmailProviderDomainSerializer(serializers.ModelSerializer):
+    """Serializer class for EmailProviderDomain."""
+
+    id = serializers.IntegerField(required=False)
+
+    class Meta:
+        extra_kwargs = {
+            "name": {
+                "validators": []
+            }
+        }
+        fields = ("id", "name", "new_domain")
+        model = models.EmailProviderDomain
+
+
+class EmailProviderSerializer(serializers.ModelSerializer):
+    """Serializer class for EmailProvider."""
+
+    domains = EmailProviderDomainSerializer(many=True, required=False)
+
+    class Meta:
+        fields = "__all__"
+        model = models.EmailProvider
+
+    def create(self, validated_data):
+        """Create provider and domains."""
+        domains = validated_data.pop("domains", None)
+        validated_data.pop("id", None)
+        provider = models.EmailProvider.objects.create(**validated_data)
+        if domains:
+            to_create = []
+            for domain in domains:
+                to_create.append(models.EmailProviderDomain(
+                    provider=provider, **domain))
+            models.EmailProviderDomain.objects.bulk_create(to_create)
+        return provider
+
+    def update(self, instance, validated_data):
+        """Update provider and domains."""
+        domains = validated_data.pop("domains", [])
+        domain_ids = [domain["id"] for domain in domains if "id" in domain]
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+        for domain in instance.domains.all():
+            if domain.id not in domain_ids:
+                domain.delete()
+            else:
+                for updated_domain in domains:
+                    if updated_domain.get("id") != domain.id:
+                        continue
+                    domain.name = updated_domain["name"]
+                    domain.new_domain = updated_domain.get("new_domain")
+                    domain.save()
+                    domains.remove(updated_domain)
+                    break
+        to_create = []
+        for new_domain in domains:
+            to_create.append(models.EmailProviderDomain(
+                name=new_domain["name"],
+                new_domain=new_domain.get("new_domain"),
+                provider=instance
+            ))
+        models.EmailProviderDomain.objects.bulk_create(to_create)
+        return instance
 
 
 class CheckProviderSerializer(serializers.Serializer):
@@ -61,7 +129,13 @@ class MailboxSerializer(serializers.ModelSerializer):
         model = Mailbox
 
 
-class MigrationSerializer(v1_serializers.MigrationSerializer):
+class MigrationSerializer(serializers.ModelSerializer):
     """Serializer class for Migration."""
 
     mailbox = MailboxSerializer()
+
+    class Meta:
+        depth = 1
+        fields = ("id", "provider", "mailbox", "username")
+        model = models.Migration
+
--- modoboa/imap_migration/api/v2/viewsets.py
+++ modoboa/imap_migration/api/v2/viewsets.py
@@ -4,15 +4,26 @@ import imaplib
 import socket
 import ssl
 
-from rest_framework import response
+from rest_framework import response, filters, mixins, permissions, viewsets
 from rest_framework.decorators import action
 
-from modoboa.imap_migration.api.v1 import viewsets as v1_viewsets
+from modoboa.lib.throttle import GetThrottleViewsetMixin
 
+from ... import models
 from . import serializers
 
 
-class EmailProviderViewSet(v1_viewsets.EmailProviderViewSet):
+class EmailProviderViewSet(GetThrottleViewsetMixin, viewsets.ModelViewSet):
+    """ViewSet class for EmailProvider."""
+
+    filter_backends = (filters.SearchFilter, )
+    permission_classes = (
+        permissions.IsAuthenticated,
+        permissions.DjangoModelPermissions
+    )
+    queryset = models.EmailProvider.objects.all().prefetch_related("domains")
+    search_fields = ("name", )
+    serializer_class = serializers.EmailProviderSerializer
 
     @action(methods=["post"], detail=False)
     def check_connection(self, request, **kwargs):
@@ -27,7 +38,7 @@ class EmailProviderViewSet(v1_viewsets.EmailProviderViewSet):
                 imaplib.IMAP4_SSL(address, port)
             else:
                 imaplib.IMAP4(address, port)
-        except (socket.error, imaplib.IMAP4.error, ssl.SSLError) as error:
+        except (socket.error, imaplib.IMAP4.error, ssl.SSLError):
             return response.Response({'status': 'failed'}, 400)
         return response.Response(status=200)
 
@@ -41,6 +52,21 @@ class EmailProviderViewSet(v1_viewsets.EmailProviderViewSet):
         return response.Response(status=200)
 
 
-class MigrationViewSet(v1_viewsets.MigrationViewSet):
-    """ Change the serializer to add the user id. """
+class MigrationViewSet(GetThrottleViewsetMixin,
+                       mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.DestroyModelMixin,
+                       viewsets.GenericViewSet):
+    """ViewSet class for Migration."""
+
+    filter_backends = (filters.SearchFilter, )
+    permission_classes = (
+        permissions.IsAuthenticated,
+        permissions.DjangoModelPermissions
+    )
+    queryset = models.Migration.objects.all().select_related(
+        "provider", "mailbox__domain")
+    search_fields = (
+        "username", "provider__name", "mailbox__address",
+        "mailbox__domain__name")
     serializer_class = serializers.MigrationSerializer
