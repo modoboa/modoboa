ab1b49a32d138d4c558f59460e2b483b18fb4cf1
Spitap <dev@asdrip.fr>
2023-08-06 19:26:57 +0200

    fixed test

--- modoboa/admin/api/v2/tests.py
+++ modoboa/admin/api/v2/tests.py
@@ -458,7 +458,10 @@ class AliasViewSetTestCase(ModoAPITestCase):
 
         data = {"address": "alias@test.com"}
         resp = self.client.post(url, data, format="json")
-        self.assertEqual(resp.status_code, 400)
+        self.assertEqual(resp.status_code, 409)
+
+        al_id = models.Alias.objects.get(address="alias@test.com").pk
+        self.assertEqual(resp.json()["id"], al_id)
 
         data = {"address": "alias2@test.com"}
         resp = self.client.post(url, data, format="json")
