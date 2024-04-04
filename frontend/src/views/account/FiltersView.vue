72fa33e1515eb99aaa0ee89cdf7e39608df8ed79
Antoine Nguyen <tonio@ngyn.org>
2024-04-04 14:58:42 +0200

    Added features

--- frontend/src/views/account/FiltersView.vue
+++ frontend/src/views/account/FiltersView.vue
@@ -19,10 +19,26 @@
     color="primary"
     icon="mdi-plus"
     size="small"
-    class="ml-4"
-    @click="showCreationForm = true"
+    class="mx-4"
+    @click="showFilterSetForm = true"
     :title="$gettext('Add filter set')"
   />
+  <v-menu v-if="currentFilterSet">
+    <template v-slot:activator="{ props }">
+      <v-btn
+        append-icon="mdi-chevron-down"
+        v-bind="props"
+        size="small"
+        color="default"
+      >
+        {{ $gettext('Actions') }}
+      </v-btn>
+    </template>
+    <MenuItems
+      :items="filterSetActions"
+      :obj="currentFilterSet"
+      />
+  </v-menu>
 </div>
 
 <div
@@ -57,7 +73,7 @@
              >
            </v-btn>
          </template>
-         <MenuItems :items="filterActions" :obj="item" />
+         <MenuItems :items="getFilterActions(item)" :obj="item" />
        </v-menu>
     </template>
   </v-data-table>
@@ -111,7 +127,9 @@ const headers = [
 ]
 
 watch(currentFilterSet, (value) => {
-  fetchFilters(value.name)
+  if (value) {
+    fetchFilters(value.name)
+  }
 })
 
 function closeFilterSetForm() {
@@ -142,11 +160,47 @@ function editFilter(filter) {
   showFilterForm.value = true
 }
 
+function enableFilter(filter) {
+  accountApi.enableFilter(currentFilterSet.value.name, filter.name).then(() => {
+    filter.enabled = true
+    busStore.displayNotification({ msg: $gettext('Filter enabled') })
+  })
+}
+
+function disableFilter(filter) {
+  accountApi.disableFilter(currentFilterSet.value.name, filter.name).then(() => {
+    filter.enabled = false
+    busStore.displayNotification({ msg: $gettext('Filter disabled') })
+  })
+}
+
 function deleteFilter(filter) {
   accountApi.deleteFilter(currentFilterSet.value.name, filter.name).then(() => {
     busStore.displayNotification({ msg: $gettext('Filter removed') })
     fetchFilters(currentFilterSet.value.name)
   })
+}
+
+async function activateFilterSet(filterSet) {
+  await accountApi.activateFilterSet(filterSet.name)
+  busStore.displayNotification({ msg: $gettext('Filter set activated') })
+}
+
+async function deleteFilterSet(filterSet) {
+  await accountApi.deleteFilterSet(filterSet.name)
+  busStore.displayNotification({ msg: $gettext('Filter set removed') })
+  currentFilterSet.value = null
+  fetchFilterSets()
+}
+
+async function downloadFilterSet(filterSet) {
+  const resp = await accountApi.downloadFilterSet(filterSet.name)
+  const blob = new Blob([resp.data], { type: 'text/plain' })
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(blob)
+  link.download = `${filterSet.name}.txt`
+  link.click()
+  URL.revokeObjectURL(link.href)
 }
 
 const filterActions = [
@@ -162,6 +216,46 @@ const filterActions = [
     color: 'red'
   }
 ]
+
+function getFilterActions(filter) {
+  const result = [...filterActions]
+  if (filter.enabled) {
+    result.push({
+      label: $gettext('Disable'),
+      icon: 'mdi-stop',
+      color: 'red',
+      onClick: disableFilter
+    })
+  } else {
+    result.push({
+      label: $gettext('Enable'),
+      icon: 'mdi-check',
+      color: 'success',
+      onClick: enableFilter
+    })
+  }
+  return result
+}
+
+const filterSetActions = ref([
+  {
+    label: $gettext('Activate filter set'),
+    color: 'success',
+    icon: 'mdi-check',
+    onClick: activateFilterSet
+  },
+  {
+    label: $gettext('Download filter set'),
+    icon: 'mdi-download',
+    onClick: downloadFilterSet
+  },
+  {
+    label: $gettext('Delete filter set'),
+    color: 'error',
+    icon: 'mdi-trash-can',
+    onClick: deleteFilterSet
+  }
+])
 
 fetchFilterSets()
 </script>
