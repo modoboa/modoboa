5fbc23eac49b4b914c4f0091f1cab41e252f8e69
Spitfireap <dev@asdrip.fr>
2024-07-12 19:52:14 +0200

    Added new extension parameters

--- modoboa/sievefilters/apps.py
+++ modoboa/sievefilters/apps.py
@@ -7,10 +7,23 @@ from django.utils.translation import gettext_lazy
 def load_settings():
     from modoboa.parameters import tools as param_tools
     from modoboa.sievefilters import forms
+    from . import app_settings
+    from .api.v2 import serializers
 
+    # Admin parameters
     param_tools.registry.add(
         "global", forms.ParametersForm, gettext_lazy("Sieve filters")
     )
+    param_tools.registry.add2(
+        "global",
+        "sievefilters",
+        gettext_lazy("Sieve filters"),
+        app_settings.SIEVEFILTERS_PARAMETERS_STRUCT,
+        serializers.SievefiltersSettingsSerializer,
+        False,
+    )
+
+    # User parameters
     param_tools.registry.add(
         "user", forms.UserSettings, gettext_lazy("Message filters")
     )
