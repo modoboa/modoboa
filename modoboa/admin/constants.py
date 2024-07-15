418f350b11cec0d184d26cb28b1211c2ffe47317
Spitfireap <dev@asdrip.fr>
2024-07-15 18:43:52 +0200

    Removed serializer check for dkim dir, added alarms on gen

--- modoboa/admin/constants.py
+++ modoboa/admin/constants.py
@@ -29,6 +29,8 @@ DKIM_KEY_LENGTHS = [
 ]
 
 DKIM_WRITE_ERROR = "DKIM path error"
+DKIM_DIR_NOT_FOUND = "DKIM directory not found"
+DKIM_ERROR = "general DKIM generation error"
 
 ALARM_OPENED = 1
 ALARM_CLOSED = 2
