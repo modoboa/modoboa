c873887bb7312e367b62babf25d936ce4ba09f75
Antoine Nguyen <tonio@ngyn.org>
2024-07-19 10:13:42 +0200

    Fixed deploy command

--- modoboa/core/checks.py
+++ modoboa/core/checks.py
@@ -1,10 +1,38 @@
-from django.core.checks import register, Info
+from django.core.checks import register, Info, Warning
 from django.conf import settings
+from django.utils.translation import gettext as _
 
 from modoboa.core.utils import generate_rsa_private_key
 
 
+W001 = Warning(
+    _(
+        "You have USE_TZ set to False, this may result in issues during "
+        "transitions between summer/winter time (ie the same local time occuring "
+        "twice due to clock change)."
+    ),
+    hint=_("Set `USE_TZ = True` in settings.py"),
+    id="modoboa.W001",
+)
+
+
 @register(deploy=True)
-def generate_rsa_private_key_check(app_configs, **kwargs):
+def check_use_tz_enabled(app_configs, **kwargs):
+    """Ensure USE_TZ is enabled in settings.py
+
+    When USE_TZ is enabled all date/times are stored in UTC.
+    Fixes #1086 - https://github.com/modoboa/modoboa/issues/1086
+    """
+    errors = []
+    if not settings.USE_TZ:
+        errors.append(W001)
+    return errors
+
+
+@register(deploy=True)
+def check_rsa_private_key_exists(app_configs, **kwargs):
+    """
+    Ensure an RSA private key exists to enable OIDC.
+    """
     if generate_rsa_private_key(settings.BASE_DIR):
-        return Info("Generated the RSA key for OIDC.")
+        return Info("An RSA private key has been generated for OIDC.")
