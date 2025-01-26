d3b2ec6b757be878ccdf4dbbdec077ec8c424b17
Antoine Nguyen <tonio@ngyn.org>
2025-01-26 10:36:27 +0100

    Added contacts settings loading

--- modoboa/contacts/apps.py
+++ modoboa/contacts/apps.py
@@ -1,4 +1,15 @@
+"""AppConfig for contacts."""
+
 from django.apps import AppConfig
+from django.utils.translation import gettext
+
+from modoboa.contacts.forms import UserSettings
+
+
+def load_settings():
+    from modoboa.parameters import tools as param_tools
+
+    param_tools.registry.add("user", UserSettings, gettext("Contacts"))
 
 
 class ModoboaContactsConfig(AppConfig):
@@ -6,3 +17,5 @@ class ModoboaContactsConfig(AppConfig):
 
     def ready(self):
         from . import handlers
+
+        load_settings()
