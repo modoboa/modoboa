b434958a79cd85beaf18be9492e36eed1bec0673
Antoine Nguyen <tonio@ngyn.org>
2025-03-26 14:24:48 +0100

    Make sure to use the right client everywhere

--- modoboa/policyd/utils.py
+++ modoboa/policyd/utils.py
@@ -9,9 +9,15 @@ from . import constants
 
 def get_redis_connection():
     """Return a client connection to Redis server."""
-    rclient = redis.Redis(
-        host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_QUOTA_DB
-    )
+    if not getattr(settings, "REDIS_SENTINEL", False):
+        rclient = redis.Redis(
+            host=settings.REDIS_HOST,
+            port=settings.REDIS_PORT,
+            db=settings.REDIS_QUOTA_DB,
+        )
+    else:
+        sentinel = redis.sentinel.Sentinel(settings.REDIS_SENTINELS, socket_timeout=0.1)
+        rclient = sentinel.master_for(settings.REDIS_MASTER, socket_timeout=0.1)
     rclient.set_response_callback("HGET", int)
     return rclient
 
