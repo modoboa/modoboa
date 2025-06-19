2d3d92ca9ada91bf29bafaf15306202814ff8f56
Antoine Nguyen <tonio@ngyn.org>
2025-06-19 17:31:04 +0200

    Improved coverage

--- modoboa/rspamd/utils.py
+++ modoboa/rspamd/utils.py
@@ -1,21 +1,15 @@
 """Utilies for modoboa.rspamd plugin."""
 
-from django.core.exceptions import AppRegistryNotReady
-
 
 def get_rspamd_options():
-    try:
-        from modoboa.core import models
+    from modoboa.core import models
 
-        rspamd_options = {}
-        localconfig = models.LocalConfig.objects.first()
-        rspamd_location = localconfig.parameters.get_value(
-            "rspamd_dashboard_location", raise_exception=False
-        )
-        if not rspamd_location:
-            return None
-        rspamd_options["location"] = rspamd_location
-        return rspamd_options
-    except AppRegistryNotReady:
-        # If the instance is not restarted after rspamd installation
+    rspamd_options = {}
+    localconfig = models.LocalConfig.objects.first()
+    rspamd_location = localconfig.parameters.get_value(
+        "rspamd_dashboard_location", raise_exception=False
+    )
+    if not rspamd_location:
         return None
+    rspamd_options["location"] = rspamd_location
+    return rspamd_options
