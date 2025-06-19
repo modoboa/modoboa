2e326d0f39ac7bd80e601d92a313ddaa6c76b986
Antoine Nguyen <tonio@ngyn.org>
2025-06-19 16:16:24 +0200

    Improved coverage

--- modoboa/core/api/v2/tests.py
+++ modoboa/core/api/v2/tests.py
@@ -534,3 +534,12 @@ class FIDOViewSetTestCase(ModoAPITestCase):
         resp = self.client.post(url, data, format="json")
         self.assertEqual(resp.status_code, 200)
         self.assertIn("tokens", resp.json())
+
+
+class CapabilitiesAPITestCase(ModoAPITestCase):
+
+    def test_get(self):
+        url = reverse("v2:capabilities")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("rspamd", response.json()["capabilities"])
