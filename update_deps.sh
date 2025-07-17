67fa90e3fdf4d48b7c78fc66f950f544e0ba4943
Adrien P <dev@asdrip.fr>
2025-07-17 11:27:47 +0200

    Some update

--- update_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pip-compile --output-file=requirements.txt pyproject.toml
-pip-compile --extra=mysql --output-file=mysql-requirements.txt pyproject.toml
-pip-compile --extra=postgresql --output-file=postgresql-requirements.txt pyproject.toml
-pip-compile --extra=ldap --output-file=ldap-requirements.txt pyproject.toml
-pip-compile --extra=dev --output-file=dev-requirements.txt pyproject.toml
-pip-compile --extra=test --output-file=test-requirements.txt pyproject.toml
