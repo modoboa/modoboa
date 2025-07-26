7b5cdbb285062afaa6ffe96b5421ee551a5dccac
Antoine Nguyen <tonio@ngyn.org>
2025-07-26 20:30:29 +0200

    Fixed various issues with dynamic redirect uris and custom theme loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,44 +4,4 @@
   </Suspense>
 </template>
 
-<script setup>
-import { useTheme } from 'vuetify'
-import themeApi from '@/api/theme'
-
-const theme = useTheme()
-
-themeApi.getTheme().then((response) => {
-  if (
-    response.data.theme_primary_color !==
-    theme.themes.value.light.colors.primary
-  ) {
-    theme.themes.value.light.colors.primary = response.data.theme_primary_color
-  }
-  if (
-    response.data.theme_primary_color_light !==
-    theme.themes.value.light.colors['primary-lighten-1']
-  ) {
-    theme.themes.value.light.colors['primary-lighten-1'] =
-      response.data.theme_primary_color_light
-  }
-  if (
-    response.data.theme_primary_color_dark !==
-    theme.themes.value.light.colors['primary-darken-1']
-  ) {
-    theme.themes.value.light.colors['primary-darken-1'] =
-      response.data.theme_primary_color_dark
-  }
-  if (
-    response.data.theme_secondary_color !==
-    theme.themes.value.light.colors.secondary
-  ) {
-    theme.themes.value.light.colors.secondary =
-      response.data.theme_secondary_color
-  }
-  if (
-    response.data.theme_label_color !== theme.themes.value.light.colors.label
-  ) {
-    theme.themes.value.light.colors.label = response.data.theme_label_color
-  }
-})
-</script>
+<script setup></script>
