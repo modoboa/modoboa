6afa2d15ac6eed18adb158afd5638baa2adab581
Antoine Nguyen <tonio@ngyn.org>
2025-09-15 13:50:59 +0200

    Apply custom theme to login page also.
    And improved theme login for frontend.

--- frontend/src/views/login/LoginCallbackView.vue
+++ frontend/src/views/login/LoginCallbackView.vue
@@ -1,30 +1,34 @@
 <script setup>
 import { onMounted } from 'vue'
 import { useAuthStore } from '@/stores'
+import { useModoboaTheme } from '@/composables/theme'
 
 const props = defineProps({
   redirectUrl: String,
 })
 
 const authStore = useAuthStore()
+const { enableTheme } = useModoboaTheme()
 
 onMounted(() => {
   authStore.completeLogin(props.redirectUrl)
 })
+
+await enableTheme()
 </script>
 
 <template>
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
       <v-col cols="auto" class="text-center">
-      <h1 class="ma-5">{{ $gettext('Attempting to log you in.') }}</h1>
-      <v-progress-circular
-        color="primary"
-        indeterminate
-        :size="128"
-        :width="12"
-        align-self="center"
-      ></v-progress-circular>
+        <h1 class="ma-5">{{ $gettext('Attempting to log you in.') }}</h1>
+        <v-progress-circular
+          color="primary"
+          indeterminate
+          :size="128"
+          :width="12"
+          align-self="center"
+        ></v-progress-circular>
       </v-col>
     </v-row>
   </v-container>
