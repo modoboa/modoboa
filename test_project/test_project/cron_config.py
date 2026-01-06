6d890d02b2349685f28c69616a65584409d7e0f0
Antoine Nguyen <tonio@ngyn.org>
2026-01-06 15:46:27 +0100

    Wrap maillog management commands with simple functions.

--- test_project/test_project/cron_config.py
+++ test_project/test_project/cron_config.py
@@ -2,7 +2,12 @@ from rq.cron import register
 
 from modoboa.core import jobs as core_jobs
 from modoboa.admin import jobs as admin_jobs
+from modoboa.maillog import jobs as maillog_jobs
 
+
+register(core_jobs.clean_logs, queue_name="modoboa", cron="0 0 * * *")
 
 register(admin_jobs.handle_mailbox_operations, queue_name="dovecot", cron="* * * * *")
-register(core_jobs.clean_logs, queue_name="modoboa", cron="0 0 * * *")
+
+register(maillog_jobs.logparser, queue_name="modoboa", cron="*/5 * * * *")
+register(maillog_jobs.update_statistics, queue_name="modoboa", cron="0 * * * *")
