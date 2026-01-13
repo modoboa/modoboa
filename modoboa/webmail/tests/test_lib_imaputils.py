7644d17b7014b0162cdc315511780eca16933e70
Christophe Chauvet <christophe.chauvet@gmail.com>
2026-01-13 22:18:10 +0100

    test: test both parameter

--- modoboa/webmail/tests/test_lib_imaputils.py
+++ modoboa/webmail/tests/test_lib_imaputils.py
@@ -19,8 +19,15 @@ class ImapUtilsTestCase(unittest.TestCase):
         result = [bytearray('(FROM "bob")', "utf8")]
         self.assertEqual(self.imap_connector.criterions, result)
 
+    def test_criterions_both_criterion_with_pattern(self):
+        """Test with both Criterion and pattern"""
+        self.imap_connector.criterions = []
+        self.imap_connector.parse_search_parameters("both", "bob")
+        result = [bytearray('(FROM "bob") OR (SUBJECT "bob")', "utf8")]
+        self.assertEqual(self.imap_connector.criterions, result)
+
     def test_criterions_one_criterion_without_pattern(self):
-        """Test with Criterion and pattern"""
+        """Test with Criterion and empty pattern"""
         self.imap_connector.criterions = []
         self.imap_connector.parse_search_parameters("SEEN", "")
         result = [bytearray("ALL", "utf8")]
