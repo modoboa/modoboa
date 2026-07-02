c7d29fbc11415dab95870ac46c6f863ccd13a9b4
Bruno <bruno-nyland@hotmail.com>
2026-07-02 13:04:40 -0400

    fix(tests): add tests for empty email address in Autoconfig and Autodiscover

--- modoboa/autoconfig/tests.py
+++ modoboa/autoconfig/tests.py
@@ -13,6 +13,16 @@ class ViewsTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertIn(b"%EMAILLOCALPART%@%EMAILDOMAIN%", resp.content)
 
+    def test_autoconfig_empty_emailaddress(self):
+        url = reverse("autoconfig:autoconfig")
+
+        resp = self.client.get(f"{url}?emailaddress=")
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(b"%EMAILLOCALPART%@%EMAILDOMAIN%", resp.content)
+
+    def test_autoconfig_with_emailaddress(self):
+        url = reverse("autoconfig:autoconfig")
+
         resp = self.client.get(f"{url}?emailaddress=test@test.com")
         self.assertEqual(resp.status_code, 200)
 
@@ -44,6 +54,25 @@ class ViewsTestCase(TestCase):
         self.assertIn(b"<LoginName>test@test.com", resp.content)
 
         resp = self.client.post(url, data=b"not xml", content_type="text/xml")
+        self.assertEqual(resp.status_code, 404)
+
+    def test_autodiscover_empty_emailaddress_xml_element(self):
+        url = reverse("autoconfig:autodiscover")
+        body = b"""<?xml version="1.0" encoding="utf-8"?>
+<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/requestschema/2006">
+  <Request>
+    <EMailAddress></EMailAddress>
+    <AcceptableResponseSchema>http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a</AcceptableResponseSchema>
+  </Request>
+</Autodiscover>"""
+
+        resp = self.client.post(url, data=body, content_type="text/xml")
+        self.assertEqual(resp.status_code, 404)
+
+    def test_autodiscover_empty_emailaddress_form_field(self):
+        url = reverse("autoconfig:autodiscover")
+
+        resp = self.client.post(url, {"EmailAddress": ""})
         self.assertEqual(resp.status_code, 404)
 
     def test_mobileconfig(self):
