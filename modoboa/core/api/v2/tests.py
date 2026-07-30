514c96de47d5743c1b821789d9e9f48c80543474
Erin of Yukis <erin-dev@yukis.eu>
2026-07-30 13:56:49 +0000

    tests(core): Allow running tests without requiring network access
    Network-based tests can be excluded using by passing the `--exclude-tag=network`
    flag to `./manage.py test`.

--- modoboa/core/api/v2/tests.py
+++ modoboa/core/api/v2/tests.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.test import override_settings
+from django.test import override_settings, tag
 from django.urls import reverse
 
 from modoboa.admin import (
@@ -543,6 +543,7 @@ class NewsFeedAPIViewTestCase(ModoAPITestCase):
         super().setUpTestData()
         factories.populate_database()
 
+    @tag("network")
     def test_get(self):
         url = reverse("v2:news-feed")
         response = self.client.get(url)
@@ -556,6 +557,7 @@ class NewsFeedAPIViewTestCase(ModoAPITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 0)
 
+    @tag("network")
     def test_custom_rss_feed(self):
         self.set_global_parameter(
             "rss_feed_url", "https://www.djangoproject.com/rss/weblog/"
